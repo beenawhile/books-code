2331e3503dcf4899367f02dd19c14453214018e2
youngmin-gwon <youngmin.gwon@gmail.com>
2022-10-28 21:54:24 +0900

    docs: move checklist to changelog.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,33 @@
-## 1.0.0
+## Check-List
 
-- Initial version.
+$5 + 10 CHF = $10 if rate is 2:1
+
+~~$5 * 2 = $10~~
+
+~~Make "amount" private~~
+
+~~Dollar side-effects?~~
+
+Money Rounding?<br/>
+
+~~equals()~~
+
+hashCode
+
+Equal null
+
+Equal object
+
+~~5 CHF * 2 = 10 CHF~~
+
+Dolllar/Franc duplication
+
+~~Common equals~~
+
+Common times
+
+~~Compare Francs with Dollars~~
+
+~~Currency?~~
+
+Delete "Franc should calculate times()" in franc_test.dart</br>
--- lib/dollar.dart
+++ lib/dollar.dart
@@ -1,21 +1,5 @@
 import 'money.dart';
 
-/// $5 + 10 CHF = $10 if rate is 2:1<br/>
-/// ~$5 * 2 = $10~<br/>
-/// ~Make "amount" private~<br/>
-/// ~Dollar side-effects?~<br/>
-/// Money Rounding?<br/>
-/// ~equals()~<br/>
-/// hashCode<br/>
-/// Equal null<br/>
-/// Equal object<br/>
-/// ~5 CHF * 2 = 10 CHF~<br/>
-/// Dolllar/Franc duplication<br/>
-/// ~Common equals~<br/>
-/// Common times<br/>
-/// ~Compare Francs with Dollars~<br/>
-/// ~Currency?~<br/>
-/// Delete "Franc should calculate times()" in franc_test.dart</br>
 class Dollar extends Money {
   Dollar(super.amount, super.currency);
 
