c9ffab2c1eceec67e318a9c8e2e8de388dee4e2a
youngmin-gwon <youngmin.gwon@gmail.com>
2023-01-01 23:23:40 +0900

    refactor(refactoring-1): extract method
    - extract volumeCredits variable calculation

--- refactoring/lib/ch1/version_2/statement.dart
+++ refactoring/lib/ch1/version_2/statement.dart
@@ -8,17 +8,12 @@ class Statement {
     String result = 'Statement for ${invoice.customer}\n';
 
     for (final performance in invoice.performances) {
-      // add volume credits
-      volumeCredits += math.max(performance.audience - 30, 0);
-
-      // add extra credit for every ten comedy attendees
-      if (_playFor(plays, performance)?.type == PlayType.comedy) {
-        volumeCredits += (performance.audience / 5).floor();
-      }
       // print line for this order
       result +=
           '\t${_playFor(plays, performance)?.name}: \$${(_amountFor(performance, plays) / 100).floor()} (${performance.audience} seats)\n';
       totalAmount += _amountFor(performance, plays);
+
+      volumeCredits += _volumeCreditFor(plays, performance);
     }
     result +=
         'Amount owed is \$${(totalAmount / 100).floor()}\nYou earned $volumeCredits credits';
@@ -51,5 +46,17 @@ class Statement {
 
   Play? _playFor(Plays plays, Performance performance) {
     return plays.getPlay(performance);
+  }
+
+  int _volumeCreditFor(Plays plays, Performance performance) {
+    // add volume credits
+    int volumeCredits = math.max(performance.audience - 30, 0);
+
+    // add extra credit for every ten comedy attendees
+    if (_playFor(plays, performance)?.type == PlayType.comedy) {
+      volumeCredits += (performance.audience / 5).floor();
+    }
+
+    return volumeCredits;
   }
 }
