3cfb2e6a6d3c377800bca903177c89edca2e0eca
youngmin-gwon <youngmin.gwon@gmail.com>
2023-01-01 23:03:46 +0900

    refactor(refactoring-1): rename return value to 'result'

--- refactoring/lib/ch1/version_2/statement.dart
+++ refactoring/lib/ch1/version_2/statement.dart
@@ -28,25 +28,25 @@ class Statement {
   }
 
   int _amountFor(Performance perf, Play? play) {
-    int thisAmount = 0;
+    int result = 0;
     switch (play?.type) {
       case PlayType.tragedy:
-        thisAmount = 40000;
+        result = 40000;
         if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+          result += 1000 * (perf.audience - 30);
         }
         break;
       case PlayType.comedy:
-        thisAmount = 30000;
+        result = 30000;
         if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+          result += 10000 + 500 * (perf.audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * perf.audience;
         break;
       default:
         throw ArgumentError('unknown type: ${play?.type}');
     }
 
-    return thisAmount;
+    return result;
   }
 }
