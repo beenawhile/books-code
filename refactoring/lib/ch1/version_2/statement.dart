766d93fdbee725775e62f1f5caecdc06635042c9
youngmin-gwon <youngmin.gwon@gmail.com>
2023-01-03 10:44:41 +0900

    refactor(refactoring-1): add new feature
    - add render html statment method

--- refactoring/lib/ch1/version_2/statement.dart
+++ refactoring/lib/ch1/version_2/statement.dart
@@ -13,8 +13,22 @@ class Statement {
       result +=
           '\t${statementData.playFor(performance)?.name}: \$${(statementData.amountFor(performance) / 100).floor()} (${performance.audience} seats)\n';
     }
-    result +=
-        'Amount owed is \$${statementData.totalAmount()}\nYou earned ${statementData.totalVolumeCredits()} credits';
+    result += 'Amount owed is \$${statementData.totalAmount()}\n';
+    result += 'You earned ${statementData.totalVolumeCredits()} credits';
+    return result;
+  }
+
+  String _renderHtml(StatementData statementData) {
+    String result = '<h1>Statement for ${statementData.customer}</h1>\n';
+    result += '<table>\n';
+    result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+    for (final performance in statementData.performances) {
+      result += ' <tr><td>${statementData.playFor(performance)?.name}</td><td>${performance.audience}</td>';
+      result += '<td>${(statementData.amountFor(performance)/100).round()}</td></tr>\n';
+    }
+    result += "</table>\n";
+    result += '<p>Amount owed is <em>\$${statementData.totalAmount()}</em></p>';
+    result += '<p>You earned <em>${statementData.totalVolumeCredits()}</em> credits</p>';
     return result;
   }
 }
